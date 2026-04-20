230a7890119685dfdd35e83f97b1050108ea3593
gujun <gujun@live.hk>
2026-04-20 22:34:12 +0800

    Add: files from start spring

---
+++ mcp-tools/src/main/java/com/junteam/ai/mcptools/McpToolsApplication.java
@@ -0,0 +1,13 @@
+package com.junteam.ai.mcptools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpToolsApplication.class, args);
+	}
+
+}
---
+++ mcp-tools/src/test/java/com/junteam/ai/mcptools/McpToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junteam.ai.mcptools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
